2a0986a96fbb93e3488d029857f5933f8d0be7d9
赵波 <zhaobo_cd@keruyun.com>
2018-11-28 20:29:45 +0800

    protobuf

--- src/main/java/com/shengsiyuan/netty/fifthexample/TextWebSocketFrameHandler.java
+++ src/main/java/com/shengsiyuan/netty/fifthexample/TextWebSocketFrameHandler.java
@@ -20,11 +20,11 @@ public class TextWebSocketFrameHandler extends SimpleChannelInboundHandler<TextW
 
 
     @Override public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("handlerAdded" + ctx.channel().id().asLongText());
+        System.out.println("handlerAdded:" + ctx.channel().id().asLongText());
     }
 
     @Override public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("handlerAdded" + ctx.channel().id().asLongText());
+        System.out.println("handlerRemoved:" + ctx.channel().id().asLongText());
     }
 
     @Override public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
